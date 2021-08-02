6aa814111874224492361a7240bf407149817018
Luiz Gavioli <luizgavioli@cofcointernational.com>
2021-08-02 18:08:08 +0100

    Added game page

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var http = require('http');
+var socketio = require('socket.io');
 
-//var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 
 var app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +28,25 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
-module.exports = app;
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+  
+// Error handler
+app.use(function (err, req, res, next) {
+  
+    // Set locals, only providing error
+    // in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') 
+            === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+  
+module.exports = { app: app, server: server };
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/newgame', function(req, res, next) {
+  res.redirect('/game.html');
 });
 
 module.exports = router;
