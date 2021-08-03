4a2da99f9e2a2c82c0a28c284b2ea646aa89633f
Luiz Gavioli <luizgavioli@cofcointernational.com>
2021-08-03 17:46:54 +0100

    Removed assets from server

--- game/src/main.js
+++ game/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import VueSocketIO from 'vue-socket-io'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueSocketIO);
 
 new Vue({
   render: h => h(App),
